e13eaad01021f5725f670bb40dd27ee56a572271
z5217527 <z5217527@ad.unsw.edu.au>
2020-04-08 19:25:31 +1000

    Update MainActivity.java

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -17,26 +17,26 @@ public class MainActivity extends AppCompatActivity {
     private Adapter mAdapter;
 
     ArrayList<Countries> countries;
-    private int[] images = {R.drawable.pic1, R.drawable.pic2,R.drawable.pic3,R.drawable.pic4,R.drawable.pic5,
-            R.drawable.pic6,R.drawable.pic7,R.drawable.pic8};
+    private int[] images = {R.drawable.pic1, R.drawable.pic2, R.drawable.pic3, R.drawable.pic4, R.drawable.pic5,
+            R.drawable.pic6, R.drawable.pic7, R.drawable.pic8};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG,"Reached onCreate Main");
+        Log.d(TAG, "Reached onCreate Main");
 
         countries = Countries.getCountries();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView rView = findViewById(R.id.rView);
+        RecyclerView rView = findViewById(R.id.recycler_view);
         rView.setHasFixedSize(true);
         rView.setLayoutManager(new LinearLayoutManager(this));
 
 
-        mAdapter = new Adapter(countries,images);
+        mAdapter = new Adapter(this, countries, images);
         rView.setAdapter(mAdapter);
     }
 
-    private void clickResponse(int position){
+    private void clickResponse(int position) {
         Log.d(TAG, "clickResponse: pressed " + position);
 
         Countries country = countries.get(position);
