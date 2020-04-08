c8ee1667d3202f8cadcfe711a1db9977112da116
z5217527 <z5217527@ad.unsw.edu.au>
2020-04-08 19:25:38 +1000

    Update quiz.java

--- app/src/main/java/com/example/assignment/quiz.java
+++ app/src/main/java/com/example/assignment/quiz.java
@@ -3,12 +3,13 @@ package com.example.assignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class quiz extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
     }
 }
